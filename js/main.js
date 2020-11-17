22caf33e590e0437455bfbf2288eae08664de921
Aleksandra Szychlinska <szychlinska@gmail.com>
2020-11-17 16:44:06 +0100

    str b responsywna

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
-    $('.menu-k-icon').click(function() {
-        console.log('something');
-        $('.main-menu').toggleClass('open');
+    $('.k-menu').click(function() {
+        console.log('klik');
     });
 
+$($('.main-menu-item').get().reverse()).each(function(index){
+    /* $(this).css({paddingLeft: '30px'}); */
+    $(this).css({paddingLeft: 15*index + 'px'});
+
 });
+
+let dancesArray = ['krakowskie', 'lubelskie', 'góralskie', 'Księstwo Warszawskie', 'imprezy', 'dzieci', 'młodzież', 'dorośli', 'rzeszowskie'];
+
+let array1 = [5, 15, 35, 6, 2, 32]
+console.log(array1.length);
+let array2 = array1.filter(myFunction);
+
+function myFunction(value, index) {
+    return index > 2;
+}
+
+console.log(array2);
+    
+})
+
+
+
