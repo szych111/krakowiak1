257281cd48f4a87d3f355d4e74fcd58e09a84ed8
Aleksandra Szychlinska <szychlinska@gmail.com>
2020-11-27 19:09:53 +0100

    menu boczne chowane na klik w doc

--- js/main.js
+++ js/main.js
@@ -1,34 +1,16 @@
-$(document).ready(function(){
-//     $('.k-menu').click(function() {
-//         console.log('klik');
-//     });
+$(document).ready(function () {
+    const $menu = $('.nav-menu-holder');
 
-// $($('.main-menu-item').get().reverse()).each(function(index){
-//     /* $(this).css({paddingLeft: '30px'}); */
-//     $(this).css({paddingLeft: 15*index + 'px'});
+    $(document).mouseup(e => {
+        if(!$menu.is(e.target)
+        && $menu.has(e.target).length === 0) {
+            $menu.removeClass('nav-menu-open');
+        }
+    });
 
-// });
-
-$('.nav-menu-button').click(function(){
-    $('.nav-menu-holder').toggleClass('open');
-
-});
-
-
-
-let dancesArray = ['krakowskie', 'lubelskie', 'góralskie', 'Księstwo Warszawskie', 'imprezy', 'dzieci', 'młodzież', 'dorośli', 'rzeszowskie'];
-
-let array1 = [5, 15, 35, 6, 2, 32]
-console.log(array1.length);
-let array2 = array1.filter(myFunction);
-
-function myFunction(value, index) {
-    return index > 2;
-}
-
-console.log(array2);
-    
-})
-
-
-
+    $('.nav-menu-button').on('click', () => {
+        $menu.toggleClass('nav-menu-open');
+    });
+        
+        
+    })
