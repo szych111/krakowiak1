d135f0bd17b9540eadb634522915f477ee35072e
Aleksandra Szychlinska <szychlinska@gmail.com>
2021-01-02 17:18:20 +0100

    z dzwiekiem

--- js/onload-modal.js
+++ js/onload-modal.js
@@ -1,9 +1,45 @@
-function openModal() {
-    document.getElementById("onloadModal").style.display = "block";
+let modal = document.getElementById('audio-modal');
+let btnOn = document.getElementById('audio-modal-btn-on');
+let btnOff = document.getElementById('audio-modal-btn-off');
+let btnMainOn = document.getElementById('audio-main-on');
+let btnMainOff = document.getElementById('audio-main-off');
+
+btnOn.onclick = function() {
+  modal.style.display = 'none'
+  document.getElementById('audio-bcg').play();
+  btnMainOn.style.opacity = 0.6;
+  btnMainOn.style.cursor = 'default';
+}
+
+btnOff.onclick = function() {
+  modal.style.display = 'none';
+  btnMainOff.style.opacity = 0.6;
+  btnMainOff.style.cursor = 'default';
+
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+    btnMainOff.style.opacity = 0.6;
+    btnMainOff.style.cursor = 'default';
   }
-  
-  function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-  }
+}
+
+btnMainOn.onclick = function(event) {
+  document.getElementById('audio-bcg').play();
+  btnMainOn.style.opacity = 0.6;
+  btnMainOn.style.cursor = 'default';
+  btnMainOff.style.opacity = 1;
+  btnMainOff.style.cursor = 'pointer';
+}
+
+btnMainOff.onclick = function(event) {
+  document.getElementById('audio-bcg').pause();
+  btnMainOff.style.opacity = 0.6;
+  btnMainOff.style.cursor = 'default';
+  btnMainOn.style.opacity = 1;
+  btnMainOn.style.cursor = 'pointer';
+}
 
   
