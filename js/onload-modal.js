1708ba3d091c882c67ee3ee4b71dce475d10497b
Aleksandra Szychlinska <szychlinska@gmail.com>
2021-01-05 10:31:53 +0100

    svg same directory

--- js/onload-modal.js
+++ js/onload-modal.js
@@ -5,15 +5,15 @@ let btnMainOn = document.getElementById('audio-main-on');
 let btnMainOff = document.getElementById('audio-main-off');
 
 btnOn.onclick = function() {
-  modal.style.display = 'none'
+  modal.style.display = 'none';
   document.getElementById('audio-bcg').play();
-  btnMainOn.style.opacity = 0.6;
+  btnMainOn.style.opacity = 0.2;
   btnMainOn.style.cursor = 'default';
 }
 
 btnOff.onclick = function() {
   modal.style.display = 'none';
-  btnMainOff.style.opacity = 0.6;
+  btnMainOff.style.opacity = 0.2;
   btnMainOff.style.cursor = 'default';
 
 }
@@ -21,14 +21,14 @@ btnOff.onclick = function() {
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = 'none';
-    btnMainOff.style.opacity = 0.6;
+    btnMainOff.style.opacity = 0.2;
     btnMainOff.style.cursor = 'default';
   }
 }
 
 btnMainOn.onclick = function(event) {
   document.getElementById('audio-bcg').play();
-  btnMainOn.style.opacity = 0.6;
+  btnMainOn.style.opacity = 0.2;
   btnMainOn.style.cursor = 'default';
   btnMainOff.style.opacity = 1;
   btnMainOff.style.cursor = 'pointer';
@@ -36,7 +36,7 @@ btnMainOn.onclick = function(event) {
 
 btnMainOff.onclick = function(event) {
   document.getElementById('audio-bcg').pause();
-  btnMainOff.style.opacity = 0.6;
+  btnMainOff.style.opacity = 0.2;
   btnMainOff.style.cursor = 'default';
   btnMainOn.style.opacity = 1;
   btnMainOn.style.cursor = 'pointer';
