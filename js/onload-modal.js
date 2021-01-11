0477dfbd5727a08ff61114adccf857e1979e055f
Aleksandra Szychlinska <szychlinska@gmail.com>
2021-01-11 11:23:13 +0100

    lang switch on homepage

--- js/onload-modal.js
+++ js/onload-modal.js
@@ -3,6 +3,8 @@ let btnOn = document.getElementById('audio-modal-btn-on');
 let btnOff = document.getElementById('audio-modal-btn-off');
 let btnMainOn = document.getElementById('audio-main-on');
 let btnMainOff = document.getElementById('audio-main-off');
+let spanPol = document.getElementById('pol');
+let spanDeu = document.getElementById('deu');
 
 btnOn.onclick = function() {
   modal.style.display = 'none';
@@ -42,4 +44,16 @@ btnMainOff.onclick = function(event) {
   btnMainOn.style.cursor = 'pointer';
 }
 
-  
+spanDeu.onclick = function(event) {
+  spanDeu.style.opacity = 0.2;
+  spanDeu.style.cursor = 'default';
+  spanPol.style.opacity = 0.8;
+  spanPol.style.cursor = 'pointer';
+} 
+
+spanPol.onclick = function(event) {
+  spanPol.style.opacity = 0.2;
+  spanPol.style.cursor = 'default';
+  spanDeu.style.opacity = 0.8;
+  spanDeu.style.cursor = 'pointer';
+} 
